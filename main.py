fa63d562c796591819914e215da0607d9bd29071
root <root@DESKTOP-DU3L5GU.localdomain>
2019-09-14 04:45:14 +0800

    v2

--- main.py
+++ main.py
@@ -2,12 +2,37 @@ import os
 import re
 import time
 import psutil
-from wxpy import *
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
+
+
+
+def mail():
+	
+	my_sender='1767376881@qq.com'  # 发件人邮箱账号
+	my_pass = 'okaburgptfjkchbi'   # 发件人邮箱密码
+	my_user ='805907631@qq.com'      # 收件人邮箱账号，我这边发送给自己
+	ret=True
+	try:
+		msg=MIMEText('快来看看吧','plain','utf-8')
+		msg['From']=formataddr(["FromRunoob",my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
+		msg['To']=formataddr(["FK",my_user])              # 括号里的对应收件人邮箱昵称、收件人邮箱账号
+		msg['Subject']="监控网络好像挂掉了"                # 邮件的主题，也可以说是标题
+		server=smtplib.SMTP_SSL("smtp.qq.com", 465)  # 发件人邮箱中的SMTP服务器，端口是25
+		server.login(my_sender, my_pass)  # 括号中对应的是发件人邮箱账号、邮箱密码
+		server.sendmail(my_sender,[my_user,],msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+		server.quit()  # 关闭连接
+	except Exception as e:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
+		print(e)
+		ret=False
+	return ret
+ 
 
 def flow():
+
 	return psutil.disk_io_counters()[3]
 
-bot = Bot(cache_path=True)
 count = 0
 while True:
 	fir = flow()
@@ -18,15 +43,13 @@ while True:
 	if num <0.3:
 		count +=1
 		if count >10:
-			try:
-				print(num)
-				my_friend = bot.friends().search('I am 乔。')[0]
-				my_friend.send('监控网络好像挂了，快来看看吧。')
-				count = 0
-				time.sleep(3600)
-			except Exception as e:
-				time.sleep(60)
-				continue	
+			print(num)
+			ret=mail()
+			while not ret:
+				if mail():
+					break
+			count = 0
+			time.sleep(3600)
 	else:
 		count = 0
 	time.sleep(10)
